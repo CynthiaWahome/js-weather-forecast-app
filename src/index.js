5398ab3b11fd3b1d7940c79f2f4b1c9d7edc0f29
cynthia <cynthiaawsajira@gmail.com>
2024-12-17 12:34:34 +0300

    [chore]: implement weather forecast api

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -42,7 +44,7 @@ function formatDate(date) {
 function searchCity(city) {
     let encodedApiKey = 'MDRhZjBmMDQ2YWRiMTJvdDRmM2RiNzYxMDI2OGZiMGY=';
     let apiKey = atob(encodedApiKey);
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -53,7 +55,17 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function getForecast(city) {
+    let encodedApiKey = 'MDRhZjBmMDQ2YWRiMTJvdDRmM2RiNzYxMDI2OGZiMGY=';
+    let apiKey = atob(encodedApiKey);
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+
+function displayForecast(response) {
+    console.log(response.data);
+
     let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     let forecastHtml = "";
 
@@ -73,6 +85,7 @@ function displayForecast() {
         </div>
         `;
     });
+    
     let forecastElement = document.querySelector('#forecast');
     forecastElement.innerHTML = forecastHtml;
 }
@@ -81,4 +94,3 @@ let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
 searchCity('Marsabit');
-displayForecast();
